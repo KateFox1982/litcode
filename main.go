55cce357049490040575d83eee44ac11f9d35dac
kate <fox_kate@list.ru>
2023-07-17 10:25:53 +0400

    Second commit

--- main.go
+++ main.go
@@ -1097,7 +1097,7 @@ import (
 //	}
 //	return key
 //}
-
+//задача только с каналами
 func main() {
 	var nums = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8}
 	result := getCouple(nums)
